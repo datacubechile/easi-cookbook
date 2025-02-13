7ce9478d3d27ec3ef7f14ddd737354f657a410f4
jondho <jonathan.hodge@csiro.au>
2025-02-13 20:25:35 +0000

    fix bug

--- tasks/gridded/finaliser.py
+++ tasks/gridded/finaliser.py
@@ -446,6 +446,8 @@ class Finalise(ArgoTask):
             sam_changed_ds = sam_changed_ds.where(~np.isnan(sam_changed_ds.dates), drop=True)
             sam_changed_df = sam_changed_ds.to_dataframe().dropna()
             sam_changed_df['geohash'] = list(map(gh.encode_from_xy, sam_changed_df.index.get_level_values('x'), sam_changed_df.index.get_level_values('y')))
+        else:
+            sam_changed_df = ""
             
         with open('/tmp/changes','w') as outfile:
             json.dump(sam_changed_df.to_json(), outfile)
